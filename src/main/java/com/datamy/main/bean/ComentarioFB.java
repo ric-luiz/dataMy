e1f0c0bb7decd99f56dbd4ad7f72fd78e4247f16
pablo <pablo@Dextter-PC.Home>
2017-06-25 22:37:31 -0300

    adição de template ao projeto e mudança da interface

--- src/main/java/com/datamy/main/bean/ComentarioFB.java
+++ src/main/java/com/datamy/main/bean/ComentarioFB.java
@@ -10,13 +10,226 @@ package com.datamy.main.bean;
  * @author pablo
  */
 public class ComentarioFB {           
-    String row_names;
+    String rowNames;
+    String postFromId;
+    String postFromName;
+    String postMessage;
+    String postCreatedTime;
+    String postType;
+    String postLink;
+    String postId;
+    String postLikesCount;
+    String postCommentsCount;
+    String postSharesCount;
+    String likesFromName;
+    String likesFromId;
+    String commentsFromId;
+    String commentsFromName;
+    String commentsMessage;
+    String commentsCreatedTime;
+    String commentsLikesCount;
+    String commentsCommentsCount;
+    String commentsId;
+    String avaliacao;
 
     public ComentarioFB() {
     }
 
-    public ComentarioFB(String row_names) {
-        this.row_names = row_names;
+    public ComentarioFB(String row_names, String post_from_id, String post_from_name, String post_message, String post_created_time, String post_type, String post_link, String post_id, String post_likes_count, String post_comments_count, String post_shares_count, String likes_from_name, String likes_from_id, String comments_from_id, String comments_from_name, String comments_message, String comments_created_time, String comments_likes_count, String comments_comments_count, String comments_id, String avaliacao) {
+        this.rowNames = row_names;
+        this.postFromId = post_from_id;
+        this.postFromName = post_from_name;
+        this.postMessage = post_message;
+        this.postCreatedTime = post_created_time;
+        this.postType = post_type;
+        this.postLink = post_link;
+        this.postId = post_id;
+        this.postLikesCount = post_likes_count;
+        this.postCommentsCount = post_comments_count;
+        this.postSharesCount = post_shares_count;
+        this.likesFromName = likes_from_name;
+        this.likesFromId = likes_from_id;
+        this.commentsFromId = comments_from_id;
+        this.commentsFromName = comments_from_name;
+        this.commentsMessage = comments_message;
+        this.commentsCreatedTime = comments_created_time;
+        this.commentsLikesCount = comments_likes_count;
+        this.commentsCommentsCount = comments_comments_count;
+        this.commentsId = comments_id;
+        this.avaliacao = avaliacao;
     }
+
+    public String getRowNames() {
+        return rowNames;
+    }
+
+    public void setRowNames(String rowNames) {
+        this.rowNames = rowNames;
+    }
+
+    public String getPostFromId() {
+        return postFromId;
+    }
+
+    public void setPostFromId(String postFromId) {
+        this.postFromId = postFromId;
+    }
+
+    public String getPostFromName() {
+        return postFromName;
+    }
+
+    public void setPostFromName(String postFromName) {
+        this.postFromName = postFromName;
+    }
+
+    public String getPostMessage() {
+        return postMessage;
+    }
+
+    public void setPostMessage(String postMessage) {
+        this.postMessage = postMessage;
+    }
+
+    public String getPostCreatedTime() {
+        return postCreatedTime;
+    }
+
+    public void setPostCreatedTime(String postCreatedTime) {
+        this.postCreatedTime = postCreatedTime;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public String getPostLink() {
+        return postLink;
+    }
+
+    public void setPostLink(String postLink) {
+        this.postLink = postLink;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getPostLikesCount() {
+        return postLikesCount;
+    }
+
+    public void setPostLikesCount(String postLikesCount) {
+        this.postLikesCount = postLikesCount;
+    }
+
+    public String getPostCommentsCount() {
+        return postCommentsCount;
+    }
+
+    public void setPostCommentsCount(String postCommentsCount) {
+        this.postCommentsCount = postCommentsCount;
+    }
+
+    public String getPostSharesCount() {
+        return postSharesCount;
+    }
+
+    public void setPostSharesCount(String postSharesCount) {
+        this.postSharesCount = postSharesCount;
+    }
+
+    public String getLikesFromName() {
+        return likesFromName;
+    }
+
+    public void setLikesFromName(String likesFromName) {
+        this.likesFromName = likesFromName;
+    }
+
+    public String getLikesFromId() {
+        return likesFromId;
+    }
+
+    public void setLikesFromId(String likesFromId) {
+        this.likesFromId = likesFromId;
+    }
+
+    public String getCommentsFromId() {
+        return commentsFromId;
+    }
+
+    public void setCommentsFromId(String commentsFromId) {
+        this.commentsFromId = commentsFromId;
+    }
+
+    public String getCommentsFromName() {
+        return commentsFromName;
+    }
+
+    public void setCommentsFromName(String commentsFromName) {
+        this.commentsFromName = commentsFromName;
+    }
+
+    public String getCommentsMessage() {
+        return commentsMessage;
+    }
+
+    public void setCommentsMessage(String commentsMessage) {
+        this.commentsMessage = commentsMessage;
+    }
+
+    public String getCommentsCreatedTime() {
+        return commentsCreatedTime;
+    }
+
+    public void setCommentsCreatedTime(String commentsCreatedTime) {
+        this.commentsCreatedTime = commentsCreatedTime;
+    }
+
+    public String getCommentsLikesCount() {
+        return commentsLikesCount;
+    }
+
+    public void setCommentsLikesCount(String commentsLikesCount) {
+        this.commentsLikesCount = commentsLikesCount;
+    }
+
+    public String getCommentsCommentsCount() {
+        return commentsCommentsCount;
+    }
+
+    public void setCommentsCommentsCount(String commentsCommentsCount) {
+        this.commentsCommentsCount = commentsCommentsCount;
+    }
+
+    public String getCommentsId() {
+        return commentsId;
+    }
+
+    public void setCommentsId(String commentsId) {
+        this.commentsId = commentsId;
+    }
+
+    public String getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(String avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    
+    
+    
+
+    
     
 }
